3ef7181bd2acadaa4cebcb86463e776b1b7a1ec7
Kevin Silva <kevsilva07@gmail.com>
2020-04-09 09:49:55 -0300

    implemented router funcionalities

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -10,24 +9,73 @@ app.use(cors());
 
 const repositories = [];
 
+const checkIfExistsProject = (request, response, next) => {
+  const { id } = request.params;
+  const project = repositories.find((el) => el.id === id);
+
+  if (!project)
+    return response
+      .status(400)
+      .json({ status: "fail", message: "Project not found" });
+
+  return next();
+};
+
+app.use("/repositories/:id", checkIfExistsProject);
+
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.status(200).json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+
+  const project = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0,
+  };
+
+  repositories.push(project);
+
+  return response.status(201).json(project);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+  const { id } = request.params;
+
+  const project = repositories.find((el) => el.id === id);
+  const projectIndex = repositories.findIndex((el) => el.id === id);
+  const updatedProject = {
+    id: project.id,
+    title,
+    url,
+    techs,
+    likes: project.likes,
+  };
+
+  repositories[projectIndex] = updatedProject;
+
+  return response.status(200).json(updatedProject);
 });
 
-app.delete("/repositories/:id", (req, res) => {
-  // TODO
+app.delete("/repositories/:id", (request, response) => {
+  const { id } = request.params;
+  const projectIndex = repositories.findIndex((el) => el.id === id);
+  repositories.splice(projectIndex, 1);
+
+  return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const project = repositories.find((el) => el.id === id);
+  project.likes = project.likes + 1;
+
+  response.status(200).json(project);
 });
 
 module.exports = app;
